eaeb6b41e1e8bf5c5df90b326950cfcc4b4b7975
Danil Ivanov <nonamenix@gmail.com>
2017-05-22 23:54:51 +0300

    Use __init__ as main point for import

--- aioviber/__init__.py
+++ aioviber/__init__.py
@@ -1,1 +1,6 @@
-
+from aioviber.bot import Bot  # noqa
+from aioviber.chat import Chat  # noqa
+from aioviber.eventtype import EventType  # noqa
+from aioviber.messagetype import MessageType  # noqa
+from aioviber.api import Api, BotConfiguration  # noqa
+from aioviber.keyboard import Keyboard, Button, ExternalLinkButton, Carousel  # noqa
