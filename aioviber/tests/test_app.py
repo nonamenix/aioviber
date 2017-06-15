879eea17acada2b6a5c73fb78929539a6c0bd6b8
Danil Ivanov <nonamenix@gmail.com>
2017-06-16 00:17:13 +0300

    Increase test coverage

--- aioviber/tests/test_app.py
+++ aioviber/tests/test_app.py
@@ -1,6 +1,34 @@
 import pytest
 
+from aioviber.app import get_app
 
-@pytest.mark.skip
-def test_ping():
-    assert False
+
+async def test_ping(test_client):
+    app = get_app(bot=None)
+
+    client = await test_client(app)
+    resp = await client.get(
+        '/ping',
+    )
+    assert resp.status == 200
+    assert 'pong' == await resp.text()
+
+
+async def test_static(test_client):
+    app = get_app(bot=None, static_serve=True)
+
+    client = await test_client(app)
+    resp = await client.get(
+        '/static/favicon.ico',
+    )
+    assert resp.status == 200
+
+
+async def test_no_static(test_client):
+    app = get_app(bot=None, static_serve=False)
+
+    client = await test_client(app)
+    resp = await client.get(
+        '/static/favicon.ico',
+    )
+    assert resp.status == 404
